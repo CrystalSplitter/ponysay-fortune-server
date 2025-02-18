6263b869acb0c776a6261492f2b4757c54928b80
CrystalSplitter <gamewhizzit@gmail.com>
2025-02-17 16:19:48 -0800

    Clean up Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -3,50 +3,60 @@
 module Main where
 
 import Blaze.ByteString.Builder (copyByteString, fromByteString)
-import Control.Concurrent
+import Control.Concurrent (
+    Chan,
+    MVar,
+    forkIO,
+    modifyMVar,
+    newChan,
+    newMVar,
+    readChan,
+    readMVar,
+    threadDelay,
+    writeChan,
+ )
 import Data.ByteString.UTF8 qualified as BU
 import Data.Time qualified as Time
 import Data.Time.Format.ISO8601 qualified as Time
 import Network.HTTP.Types (status200)
 import Network.Wai qualified as Wai
-import Network.Wai.Handler.Warp
-import System.Exit
+import Network.Wai.Handler.Warp qualified as Warp
+import System.Exit (ExitCode (..))
 import System.IO qualified as IO
 import System.Process qualified as Proc
+
+-- ------------------------------------------------------------------------------------------------
+-- Types
+-- ------------------------------------------------------------------------------------------------
 
 data PonyResult = PonyResult ExitCode BU.ByteString
 
 data CountedChannel a = CountedChannel
-    { amnt :: MVar Int
+    { ccAmount :: MVar Int
     , chan :: Chan a
     }
 
-ponyBufferSize :: Int
-ponyBufferSize = 100
-
-port :: Int
-port = 3000
-
-logS :: String -> IO ()
-logS s = do
-    currentTime <- Time.getCurrentTime
-    IO.hPutStrLn IO.stderr (Time.iso8601Show currentTime <> " -- " <> s)
+-- ------------------------------------------------------------------------------------------------
+-- Functions
+-- ------------------------------------------------------------------------------------------------
 
 main :: IO ()
 main = do
     logS $ "Listening on port " <> show port
     ponyBuffer <- do
-        myAmnt <- newMVar 0
+        amnt <- newMVar 0
         myChan <- newChan
-        pure $ CountedChannel myAmnt myChan
+        pure $ CountedChannel amnt myChan
     _threadID <- forkIO (generatorThread ponyBuffer)
-    run port (app ponyBuffer)
+    Warp.run port (app ponyBuffer)
+
+-- Web server -------------------------------------------------------------------------------------
 
 app :: CountedChannel PonyResult -> Wai.Request -> (Wai.Response -> IO a) -> IO a
 app ponyBuffer req respond = do
     logS $ "Request From " <> (show . Wai.remoteHost $ req)
     pollForMore ponyBuffer
-    ponyResult <- modifyMVar (amnt ponyBuffer) $ \a -> do
+    ponyResult <- modifyMVar (ccAmount ponyBuffer) $ \a -> do
         ponyResult <- readChan (chan ponyBuffer)
         -- In theory, after the subtraction, a could be
         -- negative. This is okay.
@@ -56,50 +66,6 @@ app ponyBuffer req respond = do
             respond $ successResponse ponyData
         PonyResult failureExitCode _ -> do
             respond $ errorResponse failureExitCode
-
-pollForMore :: CountedChannel a -> IO ()
-pollForMore channel =
-    readMVar (amnt channel)
-        >>= \count ->
-            if count > 0
-                then do
-                    pure ()
-                else do
-                    logS "Ran out of ponies... waiting"
-                    threadDelay 300_000
-                    pollForMore channel
-
-generatorThread :: CountedChannel PonyResult -> IO ()
-generatorThread ponyBuffer = do
-    count <- readMVar (amnt ponyBuffer)
-    if count < ponyBufferSize
-        then do
-            logS ("Generating pony...")
-            newPony <- generatePony
-            handleGenResult newPony
-        else do
-            threadDelay 1_000_000
-            generatorThread ponyBuffer
-  where
-    handleGenResult (Just newPony) = do
-        logS ("Success.")
-        -- We specifically don't lock the amnt along
-        -- with the channel to prevent deadlock.
-        writeChan (chan ponyBuffer) newPony
-        newPonyCount <- modifyMVar (amnt ponyBuffer) $ \a -> do
-            pure $ (a + 1, a + 1)
-        logS ("New pony added: now at " <> show newPonyCount)
-        generatorThread ponyBuffer
-    handleGenResult Nothing = do
-        logS ("Timed out.")
-        generatorThread ponyBuffer
-
-generatePony :: IO (Maybe PonyResult)
-generatePony = do
-    fortuneOutput <- Proc.readProcess "fortune" [] ""
-    ponysayOutput <- Proc.readProcess "ponysay" ["-X", "-W", "100"] fortuneOutput
-    htmlOutput <- Proc.readProcess "ansi2html" [] (ponysayOutput <> "\n\n  Refresh for a new pony fortune.")
-    pure $ Just (PonyResult ExitSuccess (BU.fromString htmlOutput))
 
 errorResponse :: ExitCode -> Wai.Response
 errorResponse exitCode =
@@ -111,3 +77,61 @@ errorResponse exitCode =
 successResponse :: BU.ByteString -> Wai.Response
 successResponse output =
     Wai.responseBuilder status200 [("Content-Type", "text/html")] $ fromByteString output
+
+pollForMore :: CountedChannel a -> IO ()
+pollForMore channel =
+    readMVar (ccAmount channel)
+        >>= \count ->
+            if count > 0
+                then pure ()
+                else do
+                    logS "Ran out of ponies... waiting"
+                    threadDelay 300_000
+                    pollForMore channel
+
+-- Generator --------------------------------------------------------------------------------------
+
+generatorThread :: CountedChannel PonyResult -> IO ()
+generatorThread ponyBuffer = do
+    count <- readMVar (ccAmount ponyBuffer)
+    if count < ponyBufferSize
+        then do
+            logS "Generating pony..."
+            newPony <- generatePony
+            handleGenResult newPony
+        else do
+            threadDelay 1_000_000
+            generatorThread ponyBuffer
+  where
+    handleGenResult (Just newPony) = do
+        logS "Success."
+        -- We specifically don't lock the ccAmount along
+        -- with the channel to prevent deadlock.
+        writeChan (chan ponyBuffer) newPony
+        newPonyCount <- modifyMVar (ccAmount ponyBuffer) $ \a -> do
+            pure $ (a + 1, a + 1)
+        logS ("New pony added: now at " <> show newPonyCount)
+        generatorThread ponyBuffer
+    handleGenResult Nothing = do
+        logS "Timed out."
+        generatorThread ponyBuffer
+
+generatePony :: IO (Maybe PonyResult)
+generatePony = do
+    fortuneOutput <- Proc.readProcess "fortune" [] ""
+    ponysayOutput <- Proc.readProcess "ponysay" ["-X", "-W", "100"] fortuneOutput
+    htmlOutput <- Proc.readProcess "ansi2html" [] (ponysayOutput <> "\n\n  Refresh for a new pony fortune.")
+    pure $ Just (PonyResult ExitSuccess (BU.fromString htmlOutput))
+
+-- Utils ------------------------------------------------------------------------------------------
+
+logS :: String -> IO ()
+logS s = do
+    currentTime <- Time.getCurrentTime
+    IO.hPutStrLn IO.stderr (Time.iso8601Show currentTime <> " -- " <> s)
+
+ponyBufferSize :: Int
+ponyBufferSize = 100
+
+port :: Int
+port = 3000
